d308daecb791559fe7eddb79a16fe5b2d42fdaf8
SotaF-2929 <280019679+SotaF-2929@users.noreply.github.com>
2026-04-29 16:44:01 +0900

    趣味を追加

--- src/pages/Hobbies.jsx
+++ src/pages/Hobbies.jsx
@@ -1,7 +1,15 @@
 function Hobbies() {
   return (
     <section className="page">
-      <h1>趣味</h1>
+    <h1>趣味</h1>
+    <div style={{ fontSize: "30px", lineHeight: "1.8" }}>
+    <div>趣味1 筋トレ</div>
+    <div>趣味2 生け花</div>
+    <div>趣味3 ボディビル</div>
+    <div>趣味4 バイオリン</div>
+    <div>これがギャップ萌えって、、、コトォ！？</div>
+    </div>
+
     </section>
   )
 }
